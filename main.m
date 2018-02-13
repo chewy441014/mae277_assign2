42cf74a35f6a3c4254b7e01b870cd820ea430860
Hosik Chae <ckmagenta@gmail.com>
2018-02-12 18:40:18 -0800

    modified to use identified model

--- main.m
+++ main.m
@@ -6,29 +6,45 @@ clc; clear; close all;
 
 %% Model Identification
 % Motor Parameters
-LinkLength = 0.0508;        % 2 inches due to mounting
-LinkMass   = 0.045;         % 45 grams
-Km         = 22.7E-3;       % 22.7 mNm/A
+LinkLength = 75.39e-3;      % 75.30 mm
+LinkMass   = 45.73-3;       % 45.73 grams
+% Km       = 22.7E-3;       % 22.7 mNm/A
 Vs         = 12;            % 12 V
-R_dd          = 8.0061;        % 8.0061 Ohm
-L          = 0.24E-3;       % 0.24 mH
+% R_dd     = 8.0061;        % 8.0061 Ohm
+% L        = 0.24E-3;       % 0.24 mH
 J_motor    = 0.00000103;    % 10.3 gcm^2
-b          = 0;             % ~0 Nms
+% b        = 0;             % ~0 Nms
 g          = 9.81;          % 9.81 m/s^2
 
 J_link     = (1/3) * LinkMass * LinkLength ^ 2;
 J_total    = J_motor + J_link;
-kappa = Vs/Km;
-tau = J_total*R_dd/(Km^2);
-T = [1/10, 1/100, 1/1000];
+% kappa = Vs/Km;
+% tau = J_total*R_dd/(Km^2);
 
+% From System Id
+% [kappa_id, tau_id] = system_id()
+kappa_id = 451.4583;
+tau_id = 0.2009;
+
+% Calc unknown parameters
+Km = Vs/kappa_id;
+Kb = Km;
+R = tau_id*Km^2 / J_total
+
+T = [1/10, 1/100, 1/1000];
 %% Dynamic Model, Matrix Representation
 % Continuous Time Model LTI State Space System
 % Inverted Pendulum
-A = [0, 1; (3*g)/(2*J_total), -3*(Km^2)/(LinkMass*(J_total^2)*R_dd)];
-B = [0; 3*Km*Vs/(LinkMass*(J_total^2)*R_dd)];
-C = [1, 0];
-D = 0;
+% A = [0, 1; (3*g)/(2*J_total), -3*(Km^2)/(LinkMass*(J_total^2)*R_dd)];
+% B = [0; 3*Km*Vs/(LinkMass*(J_total^2)*R_dd)];
+% C = [1, 0];
+% D = 0;
+
+A = [                   0,                                      1; 
+     (3*g)/(2*LinkLength), (-3*Km*Kb)/(LinkMass*(LinkLength^2)*R)];
+B = [                   0; ( 3*Km*Vs)/(LinkMass*(LinkLength^2)*R)];
+C = [                   1,                                      0];
+D =                                                             0;
 
 sys_c = ss(A,B,C,D);
 
